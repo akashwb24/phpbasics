64ebdae0dda6de55939e2acad141cf70f911fd2f
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-07-11 16:24:01 +0530

    started php basics

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h1>Hello World</h1>";
+
+//single line comment
+
+#comment
+$name = "Akash";
+/*multi
+line
+comment
+*/
+
+//var_dump([2, 3, 5, 5]);
+
+var_dump(strpos("$name", "a"));
